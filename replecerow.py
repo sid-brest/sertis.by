31337a9c75c20e6bab33016dc3a61e409fd7dcb7
Dzmitry Struneuski <isidbrest@gmail.com>
2023-11-14 09:53:22 +0300

    add pricing

--- replecerow.py
+++ replecerow.py
@@ -3,7 +3,7 @@ import os
 # Define the folder path and the rows to replace
 folder_path = "./"
 rows_to_replace = [ 
-    ("<span>Информация</span>","<span>Заказчику</span>"),
+    ("javascript:void(0)\">Оплата услуг","pricing.html\">Оплата работ"),
     
 ]
 
