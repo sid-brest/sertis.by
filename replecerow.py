7ef094ba7dc1c8eaf50baa94f07a740613a9f07f
Dzmitry Struneuski <isidbrest@gmail.com>
2023-11-14 11:15:13 +0300

    add get started phone button

--- replecerow.py
+++ replecerow.py
@@ -3,7 +3,7 @@ import os
 # Define the folder path and the rows to replace
 folder_path = "./"
 rows_to_replace = [ 
-    ("javascript:void(0)\">Жалобы и апелляции","complaint.html\">Жалобы и апелляции"),
+    ("<a href=\"portfolio.html\"><span>Заказчику</span>","<a href=\"#\"><span>Заказчику</span>"),
     
 ]
 
