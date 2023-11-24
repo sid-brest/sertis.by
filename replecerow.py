2ce97672d2ed120c080e77dd1a0f25b7c64223af
sid-brest <isidbrest@gmail.com>
2023-11-24 09:19:56 +0300

    modified:   replecerow.py

--- replecerow.py
+++ replecerow.py
@@ -8,7 +8,7 @@ rows_to_replace = [
 ]
 # Iterate over all files in the folder
 for filename in os.listdir(folder_path):
-    if filename.endswith(".html"):
+    if filename.endswith("html"):
         file_path = os.path.join(folder_path, filename)
         
         # Read the contents of the file
