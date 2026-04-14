f09a17ab43b3c132bf6640b8d591c20915c67ee7
Dzmitry Struneuski <isidbrest@gmail.com>
2026-04-14 22:46:14 +0300

    new file:   vyshestvoyashchaya.html

--- repleceblock.py
+++ repleceblock.py
@@ -7,22 +7,7 @@ folder_path = "./"
 # НАСТРОЙКА: старый и новый код для замены
 # ============================================================
 
-old_code = """<ul>
-                <li><a href="team.html">Руководство</a></li>
-                <li><a href="structure.html">Структура</a></li>
-                <li><a href="about.html">О нас</a></li>
-                <li><a href="history.html">История</a></li>
-                <li><a href="goals.html">Цели и задачи</a></li>
-                <li><a href="requisites.html">Реквизиты</a></li>
-                <li><a href="certificates.html">Аттестаты и сертификаты</a></li>
-                <li><a href="corruption.html">Противодействие коррупции</a></li>
-                <li><a href="idiology.html">Идеологическая работа</a></li>
-                <li><a href="procedure.html">Административные процедуры</a></li>
-                <li><a href="feedback.html">Отзывы</a></li>
-              </ul>"""
-
-new_code = """<ul>
-                <li><a href="team.html">Руководство</a></li>
+old_code = """  <li><a href="team.html">Руководство</a></li>
                 <li><a href="structure.html">Структура</a></li>
                 <li><a href="about.html">О нас</a></li>
                 <li><a href="history.html">История</a></li>
@@ -33,12 +18,26 @@ new_code = """<ul>
                 <li><a href="idiology.html">Идеологическая работа</a></li>
                 <li><a href="procedure.html">Административные процедуры</a></li>
                 <li><a href="profsoyuz.html">Профсоюзная организация</a></li>
-                <li><a href="feedback.html">Отзывы</a></li>
-              </ul>"""
+                <li><a href="feedback.html">Отзывы</a></li>"""
+
+new_code = """ <li><a href="team.html">Руководство</a></li>
+                <li><a href="structure.html">Структура</a></li>
+                <li><a href="about.html">О нас</a></li>
+                <li><a href="history.html">История</a></li>
+                <li><a href="goals.html">Цели и задачи</a></li>
+                <li><a href="requisites.html">Реквизиты</a></li>
+                <li><a href="certificates.html">Аттестаты и сертификаты</a></li>
+                <li><a href="corruption.html">Противодействие коррупции</a></li>
+                <li><a href="idiology.html">Идеологическая работа</a></li>
+                <li><a href="procedure.html">Административные процедуры</a></li>
+                <li><a href="profsoyuz.html">Профсоюзная организация</a></li>
+                <li><a href="vyshestvoyashchaya.html">Вышестоящая организация</a></li>
+                <li><a href="feedback.html">Отзывы</a></li>"""
 
 # Уникальная строка для проверки, была ли замена уже сделана
 already_replaced_marker = (
-    """<li><a href="profsoyuz.html">Профсоюзная организация</a></li>"""
+    """<li><a href="vyshestvoyashchaya.html">Вышестоящая организация</a></li>
+"""
 )
 
 # ============================================================
@@ -51,7 +50,7 @@ files_no_match = 0
 errors = 0
 
 for filename in os.listdir(folder_path):
-    if not filename.endswith(".shtml"):
+    if not filename.endswith(".html"):
         continue
 
     file_path = os.path.join(folder_path, filename)
