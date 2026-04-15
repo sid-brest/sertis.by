47d90dfef5984f3b4bce982920585cfb5b246123
Dzmitry Struneuski <isidbrest@gmail.com>
2026-04-15 22:54:34 +0300

    munu order fix

--- repleceblock.py
+++ repleceblock.py
@@ -18,26 +18,26 @@ old_code = """  <li><a href="team.html">Руководство</a></li>
                 <li><a href="idiology.html">Идеологическая работа</a></li>
                 <li><a href="procedure.html">Административные процедуры</a></li>
                 <li><a href="profsoyuz.html">Профсоюзная организация</a></li>
-                <li><a href="feedback.html">Отзывы</a></li>"""
-
-new_code = """ <li><a href="team.html">Руководство</a></li>
-                <li><a href="structure.html">Структура</a></li>
-                <li><a href="about.html">О нас</a></li>
-                <li><a href="history.html">История</a></li>
-                <li><a href="goals.html">Цели и задачи</a></li>
-                <li><a href="requisites.html">Реквизиты</a></li>
-                <li><a href="certificates.html">Аттестаты и сертификаты</a></li>
-                <li><a href="corruption.html">Противодействие коррупции</a></li>
-                <li><a href="idiology.html">Идеологическая работа</a></li>
-                <li><a href="procedure.html">Административные процедуры</a></li>
-                <li><a href="profsoyuz.html">Профсоюзная организация</a></li>
                 <li><a href="vyshestvoyashchaya.html">Вышестоящая организация</a></li>
                 <li><a href="feedback.html">Отзывы</a></li>"""
 
+new_code = """ <li><a href="vyshestvoyashchaya.html">Вышестоящая организация</a></li>
+  <li><a href="team.html">Руководство</a></li>
+  <li><a href="structure.html">Структура</a></li>
+  <li><a href="about.html">О нас</a></li>
+  <li><a href="history.html">История</a></li>
+  <li><a href="goals.html">Цели и задачи</a></li>
+  <li><a href="certificates.html">Аттестаты и сертификаты</a></li>
+  <li><a href="profsoyuz.html">Профсоюзная организация</a></li>
+  <li><a href="idiology.html">Идеологическая работа</a></li>
+  <li><a href="corruption.html">Противодействие коррупции</a></li>
+  <li><a href="procedure.html">Обращения граждан и юрлиц</a></li>
+  <li><a href="feedback.html">Отзывы</a></li>
+  <li><a href="requisites.html">Реквизиты</a></li>"""
+
 # Уникальная строка для проверки, была ли замена уже сделана
 already_replaced_marker = (
-    """<li><a href="vyshestvoyashchaya.html">Вышестоящая организация</a></li>
-"""
+    """<li><a href="procedure.html">Обращения граждан и юрлиц</a></li>"""
 )
 
 # ============================================================
@@ -50,7 +50,7 @@ files_no_match = 0
 errors = 0
 
 for filename in os.listdir(folder_path):
-    if not filename.endswith(".html"):
+    if not filename.endswith(".shtml"):
         continue
 
     file_path = os.path.join(folder_path, filename)
